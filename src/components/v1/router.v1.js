619e3f5fee5fd7fdc0d7a4f67001c14f8899982c
Camilo Acevedo <camilo-mac@Camilos-MacBook-Pro.local>
2021-08-29 17:42:53 -0500

    Added new router for main router v1

--- src/components/v1/router.v1.js
+++ src/components/v1/router.v1.js
@@ -1,5 +1,11 @@
-// new router
-// new router 2
-// new comment from brayan user
-// new comment from brayan user
-// new comment from brayan user with threee enviroments
+import pulpomaticOpenAidRouter from './pulpomatic-openaid/router';
+import authenticationRouter from './authentication/router';
+import userRouter from './user/router';
+
+const routerV1 = (expressApplication) => {
+  expressApplication.use('/api/v1/user', userRouter);
+  expressApplication.use('/api/v1/authentication', authenticationRouter);
+  expressApplication.use('/api/v1/pulpomatic-aid', pulpomaticOpenAidRouter);
+};
+
+export default routerV1;
