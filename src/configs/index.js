db0da349a473b77e1319ed7ea2f7ef0b63f0b82e
Camilo Acevedo <camilo-mac@Camilos-MacBook-Pro.local>
2021-08-29 17:42:17 -0500

    Added new env variables

--- src/configs/index.js
+++ src/configs/index.js
@@ -12,4 +12,9 @@ export default {
   REDIS_URI_PRODUCTION: process.env.REDIS_URI_PRODUCTION || '',
   REDIS_URI_STAGING: process.env.REDIS_URI_STAGING || '',
   REDIS_URI_DEVELOP: process.env.REDIS_URI_DEVELOP || '',
+  AUTH_SECRET: process.env.AUTH_SECRET || '',
+  TOKEN_EXPIRATION_TIME: process.env.TOKEN_EXPIRATION_TIME || '',
+  CRYPTO_SECRET: process.env.CRYPTO_SECRET || '',
+  OPENAID: process.env.OPENAID || '',
+  REDIS_PASSWORD: process.env.REDIS_PASSWORD || '',
 };
