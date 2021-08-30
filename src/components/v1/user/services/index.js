aa8e09f2e0a415fec9013aae41459c466af15a65
Camilo Acevedo <camilo-mac@Camilos-MacBook-Pro.local>
2021-08-30 16:37:42 -0500

    Updated createNewUser service function. Added validation to check if username is already in use

--- src/components/v1/user/services/index.js
+++ src/components/v1/user/services/index.js
@@ -1,12 +1,16 @@
 import cryptoJs from 'crypto-js';
 import boom from '@hapi/boom';
 
-import newUser from '../DAO';
+import userDao from '../DAO';
 import env from '../../../../configs';
 
 const createNewUser = async ({ username, password }) => {
+  const validateUniqueUsername = await userDao.findUserByUsername(username);
+
+  if (validateUniqueUsername) throw boom.badRequest('Username already exists');
+
   const encryptedPassword = cryptoJs.AES.encrypt(password, env.CRYPTO_SECRET);
-  const createdUser = await newUser({ username, password: encryptedPassword });
+  const createdUser = await userDao.newUser({ username, password: encryptedPassword });
 
   if (!createdUser) throw boom.internal('Error trying to create new user');
   return createdUser;
