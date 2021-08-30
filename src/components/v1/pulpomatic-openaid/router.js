b1ac8f83836da490fe0dec16b8327f1b46339cf1
Camilo Acevedo <camilo-mac@Camilos-MacBook-Pro.local>
2021-08-29 22:54:49 -0500

    Added middleware to route

--- src/components/v1/pulpomatic-openaid/router.js
+++ src/components/v1/pulpomatic-openaid/router.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import asyncHandler from '../../../utils/middlewares/asyncHandler';
 import requestSchemaHandler from '../../../utils/middlewares/requestSchemaHandler';
+import validateCountryCode from '../../../utils/middlewares/countryCodeValidation';
 import loggedIn from '../../../utils/middlewares/autheticationHandler';
 import getResourcesLastFiveYears from './controllers';
 
@@ -9,6 +10,6 @@ import getResourcesByCountryCodeAndLastFiveYearsSchema from './schemas';
 
 const router = Router();
 
-router.get('/', loggedIn, requestSchemaHandler(getResourcesByCountryCodeAndLastFiveYearsSchema, 'query'), asyncHandler(getResourcesLastFiveYears));
+router.get('/', loggedIn, validateCountryCode, requestSchemaHandler(getResourcesByCountryCodeAndLastFiveYearsSchema, 'query'), asyncHandler(getResourcesLastFiveYears));
 
 export default router;
