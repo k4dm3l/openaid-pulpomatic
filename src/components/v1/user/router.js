dc504808957b4837c0c346d204fd5d218a1fb861
Camilo Acevedo <camilo-mac@Camilos-MacBook-Pro.local>
2021-10-09 20:54:58 -0500

    Testing new package

--- src/components/v1/user/router.js
+++ src/components/v1/user/router.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 
+import { getAuditInformation } from 'trackology';
 import asyncHandler from '../../../utils/middlewares/asyncHandler';
 import requestSchemaHandler from '../../../utils/middlewares/requestSchemaHandler';
 import loggedIn from '../../../utils/middlewares/autheticationHandler';
@@ -9,6 +10,11 @@ import createUserSchema from './schemas';
 
 const router = Router();
 
+router.get('/', getAuditInformation, (req, res) => {
+  res.status(200).json({
+    audit: req.audit || {},
+  });
+});
 router.post('/', loggedIn, requestSchemaHandler(createUserSchema), asyncHandler(createUser));
 
 export default router;
